2a064e349a42a08ba74b78e88e2240dcc2cc721c
fanxiaoli <312889426@qq.com>
2017-06-16 10:41:35 +0800

    使用自定义控件在屏幕中绘制一条虚线

---
+++ app/src/main/java/fanxiaoli/ex042/dashedLine.java
@@ -0,0 +1,33 @@
+package fanxiaoli.ex042;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PathEffect;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class dashedLine extends View
+{
+    public dashedLine(Context context, AttributeSet attributeSet)
+    {
+        super(context,attributeSet);
+    }
+    @Override
+    protected  void onDraw(Canvas canvas)
+    {
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.BLUE);
+        Path path = new Path();
+        path.moveTo(0,10);
+        path.lineTo(480,10);
+        PathEffect effects = new DashPathEffect(new float[]{5,5,5,5},1);
+        paint.setPathEffect(effects);
+        canvas.drawPath(path,paint);
+
+    }
+}
